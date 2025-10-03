1d94331e27e911829f4618d05ee16be76f09f83f
Van Dixon <vandixon@hotmail.com>
2025-10-03 16:07:30 -0500

    Ch 1 2 SwiftUI Print, Buttons, Var, and @State with Prof. Gallaugher

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,18 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = "I Am A Programmer!"
+    
     var body: some View {
         
         VStack {
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
-                .foregroundStyle(.purple)
-            Text("You Are Awesome!")
+                .foregroundStyle(.orange)
+            Text(message)
                 .font(.largeTitle)
                 .fontWeight(.bold)
-                .foregroundStyle(.green)
-        //Changes
+                .foregroundStyle(.red)
+            Button ("Click Me"){
+                message = "Awesome!"
+            }
         }
         .padding()
     }

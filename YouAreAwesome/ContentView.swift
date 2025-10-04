273d72c87dbbef93ff2ba43ee5b6902ea334148f
Van Dixon <vandixon@hotmail.com>
2025-10-03 20:30:07 -0500

    Added Frames for Swift image, changed text, and added to buttons in Hstack.

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,21 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var message = "I Am A Programmer!"
-    
     var body: some View {
         
         VStack {
+            
+            Spacer()
+            
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
+                .frame(width: 200, height: 200)
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.bold)
-                .foregroundStyle(.red)
-            Button ("Click Me"){
-                message = "Awesome!"
+                .fontWeight(.ultraLight)
+                .foregroundStyle(.black)
+            
+            Spacer()
+            
+            HStack {
+                Button ("Awesome!"){
+                    message = "Awesome!"
+                }
+                
+                Button ("Great!"){
+                    message = "Great!"
+                }
+                
             }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
         }
         .padding()
     }
